f8e4722aac9e78ca08b2aefb690575b7fa30743f
tamaokishie <shie0041@gmail.com>
2025-02-04 22:55:21 +0900

    fix message

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 export const App = () => {
+
+  const [message, setMessage] = useState<string | null>(null);
+
   const handleClick = async () => {
     try {
       const response = await fetch('http://localhost:8000/hello', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
+        method: 'GET'
       });
-
+  
       const data = await response.json();
-      console.log('Response:', data); // { hello: "world!" } が返るはず
+      console.log('Response:', data);
+
+      setMessage(data.helllo);
+
     } catch (error) {
       console.error('Error:', error);
+      setMessage("エラーが発生しました");
     }
   };
+  
 
   return (
     <>
@@ -24,6 +29,7 @@ export const App = () => {
         className="button"
         onClick={handleClick}>Hello
       </button>
+      {message && <p>受信したメッセージ: {message}</p>}
     </>
   );
 };
